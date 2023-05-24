d0efdc95fd38ff0f0374037769d9a27531e6dbfb
Julia Koroleva <koroleva21.jv@gmail.com>
2023-05-24 22:17:20 +0300

    fix

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -13,6 +13,7 @@ public class Program
         string n;
         int val;
         string text;
+        int maxLngsText = 40;
 
         do
         {
@@ -21,24 +22,39 @@ public class Program
             n = (Console.ReadLine());
             if (!int.TryParse(n, out val))
             {
+                Console.WriteLine("Вы ввели не корректное значение");
                 continue;
             }
-
+            else if (val < 1 || val > 6)
+            {
+                Console.WriteLine("Значение должно быть от 1 до 6");
+            }
         }
+        while (!(val >= 1 && val <= 6));
 
-        while (!(val > 1 && val <= 6));
 
         do
         {
             Console.WriteLine("введите произвольный текст:");
             text = Console.ReadLine();
-            Str(val, text);
-            //StrTrim();
+            if (text.Length > (maxLngsText - val))
+            {
+                Console.WriteLine($"Текст не может быть длиннее {maxLngsText - val}");
+            }
+            else if (string.IsNullOrWhiteSpace(text))
+            {
+                Console.WriteLine($"Текст состоит только из пробелов");
+            }
+            else
+            {
+                Str(val, text);
+                Console.ReadKey();
+            }
 
         }
-        while (string.IsNullOrWhiteSpace(text));
-
+        while (true);
     }
+
     public static void Str(int n, string text)
     {
         int strLength = text.Length + (n * 2);
@@ -72,8 +88,8 @@ public class Program
         }
         OneStr(strLength);
         StrX(strLength);
-
     }
+
     public static void StrPlusTrim(int strLength, bool even)
     {
         Console.Write(plus);
@@ -88,25 +104,23 @@ public class Program
                 Console.Write(plus);
             }
             even = !even;
-
         }
         Console.Write(plus);
         Console.WriteLine();
-
     }
+
     public static void StrX(int strLength)
     {
         for (int i = 0; i < strLength; i++)
         {
             char[] strArray = (plus.PadRight(strLength - 1) + plus).ToCharArray();
-            strArray[i ] = '+';
+            strArray[i] = '+';
             strArray[strLength - (i + 1)] = '+';
             Console.WriteLine(new string(strArray));
 
         }
         OneStr(strLength);
     }
-
 
     public static void OneStr(int strLength)
     {
@@ -117,5 +131,4 @@ public class Program
         }
         Console.WriteLine();
     }
-
 }
