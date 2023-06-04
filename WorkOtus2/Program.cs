56667a360807f4fa7743d2577e9b32f34fc02c15
Yulia <julia.koroleva@ics-it.ru>
2023-06-04 22:57:23 +0300

    fix

--- WorkOtus2/Program.cs
+++ WorkOtus2/Program.cs
@@ -3,83 +3,83 @@ using System.Collections;
 class Program
 {
 	const int elem = 1000000;
-    private static void Main(string[] args)
-    {
+	private static void Main(string[] args)
+	{
 		List<int> list = new List<int>(elem);
 		ArrayList arrayList = new ArrayList();
 		var linkedList = new LinkedList<int>();
 		Print(list);
 		Print(arrayList);
 		Print(linkedList);
-    }
+	}
 	public static void Print(object list)
 	{
 		Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
+		stopwatch.Start();
 		for (int i = 0; i < elem; i++)
-        {
-			switch(list)
+		{
+			switch (list)
 			{
 				case List<int>:
 					((List<int>)list).Add(i);
-				break;
+					break;
 				case ArrayList:
 					((ArrayList)list).Add(i);
-				break;
+					break;
 				case LinkedList<int>:
 					((LinkedList<int>)list).AddLast(i);
-				break;
+					break;
 			}
-			if(i % 777 == 0)
-					{
-						Console.WriteLine($"Число {i} делится без остатка на 777");
-					}
-        }
+			if (i % 777 == 0)
+			{
+				Console.WriteLine($"Число {i} делится без остатка на 777");
+			}
+		}
 		Console.WriteLine($"Выполнение операции {list.GetType().Name}: {stopwatch.ElapsedMilliseconds} m/s");
 		SearchPrint(list);
-        stopwatch.Stop();
+		stopwatch.Stop();
 	}
-	
-    public static void SearchPrint(Object obj)
-    {
-        int search = 496753;
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
-        switch (obj)
-        {
-            case List<int>:
 
-                for (int i = 0; i < ((List<int>)obj).Count; i++)
-                {
-                    if (i == search)
-                    {
-                        Console.WriteLine("Элемент 496753  найден");
-                        break;
-                    }
-                }
-                break;
-            case ArrayList:
-                for (int i = 0; i < ((ArrayList)obj).Count; i++)
-                {
-                    if (i == search)
-                    {
-                        Console.WriteLine("Элемент 496753  найден");
-                        break;
-                    }
-                }
-                break;
-            case LinkedList<int>:
-                for (int i = 0; i < ((LinkedList<int>)obj).Count; i++)
-                {
-                    if (i == search)
-                    {
-                        Console.WriteLine("Элемент 496753  найден");
-                        break;
-                    }
-                }
-                break;
-        }
-        stopwatch.Stop();
-        Console.WriteLine($"Выполнение операции {obj.GetType().Name} Search: {stopwatch.ElapsedMilliseconds} m/s");
-    }
+	public static void SearchPrint(Object obj)
+	{
+		int search = 496753;
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
+		switch (obj)
+		{
+			case List<int>:
+
+				for (int i = 0; i < ((List<int>)obj).Count; i++)
+				{
+					if (i == search)
+					{
+						Console.WriteLine("Элемент 496753  найден");
+						break;
+					}
+				}
+				break;
+			case ArrayList:
+				for (int i = 0; i < ((ArrayList)obj).Count; i++)
+				{
+					if (i == search)
+					{
+						Console.WriteLine("Элемент 496753  найден");
+						break;
+					}
+				}
+				break;
+			case LinkedList<int>:
+				for (int i = 0; i < ((LinkedList<int>)obj).Count; i++)
+				{
+					if (i == search)
+					{
+						Console.WriteLine("Элемент 496753  найден");
+						break;
+					}
+				}
+				break;
+		}
+		stopwatch.Stop();
+		Console.WriteLine($"Выполнение операции {obj.GetType().Name} Search: {stopwatch.ElapsedMilliseconds} m/s");
+	}
 }
---
+++ WorkOtus4/Program.cs
@@ -0,0 +1,73 @@
+﻿class Program
+{
+	private static void Main(string[] args)
+	{
+		/*var s = new Stack("a", "b", "c");
+		//size = 3, Top = 'c'
+		Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+		var deleted = s.Pop();
+		// Извлек верхний элемент 'c' Size = 2
+		Console.WriteLine($"Извлек верхний элемент '{deleted}' Size = {s.Size}");
+		s.Add("d");
+		// size = 3, Top = 'd'
+		Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+		s.Pop();
+		s.Pop();
+		s.Pop();
+		// size = 0, Top = null
+		Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
+		s.Pop();*/
+		var s = new Stack("a", "b", "c");
+		s.Merge(new Stack("1", "2", "3"));
+		// в стеке s теперь элементы - "a", "b", "c", "3", "2", "1" <- верхний
+		Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+	}
+}
+class Stack
+{
+	List<string> dictionarystr = new List<string>();
+	public int Size { get; set; } = 0;
+	public string Top { get; set; } = null;
+	public void Add(string elem)
+	{
+		dictionarystr.Add(elem);
+		Size++;
+		Top = elem;
+	}
+	public string Pop()
+	{
+		if (Size == 0)
+			throw new Exception("Стек пустой");
+		dictionarystr.RemoveAt(Size - 1);
+		Size--;
+		string retTopValue = Top;
+		if (Size > 0)
+			Top = dictionarystr[Size - 1];
+		else
+			Top = null;
+		return retTopValue;
+	}
+	public Stack(params string[] par)
+	{
+		for (int i = 0; i < par.Length; i++)
+		{
+			Add(par[i]);
+		}
+	}
+}
+
+static class StackExtensions
+{
+	/// <summary>
+	/// Метод предназначен для перегрузки данных в обратном порядке из одного стека в другой 
+	/// </summary>
+	/// <param name="count">Количество</param>
+	/// <returns></returns>	
+	public static void Merge(this Stack s1, Stack s2)
+	{
+		while (s2.Size != 0)
+		{
+			s1.Add(s2.Pop());
+		}
+	}
+}
