edb82ce214f2596e5880f6e64f94793aeffcd8b8
Yulia <julia.koroleva@ics-it.ru>
2023-06-07 23:07:10 +0300

    fix

--- WorkOtus4/Program.cs
+++ WorkOtus4/Program.cs
@@ -2,7 +2,7 @@
 {
 	private static void Main(string[] args)
 	{
-		/*var s = new Stack("a", "b", "c");
+		var s = new Stack("a", "b", "c");
 		//size = 3, Top = 'c'
 		Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
 		var deleted = s.Pop();
@@ -16,23 +16,47 @@
 		s.Pop();
 		// size = 0, Top = null
 		Console.WriteLine($"size = {s.Size}, Top = {(s.Top == null ? "null" : s.Top)}");
-		s.Pop();*/
+		s.Pop();
 		//var s = new Stack("a", "b", "c");
 		//s.Merge(new Stack("1", "2", "3"));
 		// в стеке s теперь элементы - "a", "b", "c", "3", "2", "1" <- верхний
 		//Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
-		var s = Stack.Concat(new Stack("a", "b", "c"), new Stack("1", "2", "3"), new Stack("А", "Б", "В"));
-		Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
+		//var s = Stack.Concat(new Stack("a", "b", "c"), new Stack("1", "2", "3"), new Stack("А", "Б", "В"));
+		//Console.WriteLine($"size = {s.Size}, Top = '{s.Top}'");
 	}
 }
 class Stack
 {
-	List<string> dictionarystr = new List<string>();
+	class StackItem
+	{
+		public string value;
+    	public StackItem prevItem;
+
+		public StackItem()
+		{
+			value = null;
+			prevItem = null;
+		}
+		public StackItem(string val, StackItem prev)
+		{
+			value = val;
+			prevItem = prev;
+		}
+
+	}
+	StackItem stki = new StackItem();
+	//List<string> dictionarystr = new List<string>();
 	public int Size { get; set; } = 0;
 	public string Top { get; set; } = null;
 	public void Add(string elem)
 	{
-		dictionarystr.Add(elem);
+		
+		//Реализация под List
+		//dictionarystr.Add(elem);
+
+		//Реализация под StackItem
+		stki = new StackItem(elem,stki);
+		
 		Size++;
 		Top = elem;
 	}
@@ -40,11 +64,13 @@ class Stack
 	{
 		if (Size == 0)
 			throw new Exception("Стек пустой");
-		dictionarystr.RemoveAt(Size - 1);
+		stki = stki.prevItem;
+		//dictionarystr.RemoveAt(Size - 1);
 		Size--;
 		string retTopValue = Top;
 		if (Size > 0)
-			Top = dictionarystr[Size - 1];
+			//Top = dictionarystr[Size - 1];
+			Top = stki.value;
 		else
 			Top = null;
 		return retTopValue;
