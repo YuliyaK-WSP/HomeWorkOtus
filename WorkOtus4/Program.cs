ed8b2928501320d3781fe3e3751f87b4717ad2ac
Yulia <julia.koroleva@ics-it.ru>
2023-06-14 11:45:06 +0300

    fix

--- WorkOtus4/Program.cs
+++ WorkOtus4/Program.cs
@@ -94,20 +94,4 @@ class Stack
 	}
 
 }
-// Дополнительное задание 1
-static class StackExtensions
-{
-	/// <summary>
-	/// Метод предназначен для перегрузки данных в обратном порядке из одного стека в другой 
-	/// </summary>
-	/// <param name="count">Количество</param>
-	/// <returns></returns>	
-	public static void Merge(this Stack s1, Stack s2)
-	{
-		while (s2.Size != 0)
-		{
-			s1.Add(s2.Pop());
-		}
-	}
-}
 
