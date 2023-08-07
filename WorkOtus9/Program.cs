5f3b3f66d8d563735030357e1e109081bea05884
yulia <koroleva21.jv@gmail.com>
2023-08-07 23:26:36 +0300

    fix

--- WorkOtus9/Program.cs
+++ WorkOtus9/Program.cs
@@ -8,9 +8,7 @@ internal class Program
 		//downloader.OnImageCompleted;
 		downloader.ImageStarted += DisplayMessage;
 		downloader.ImageCompleted += DisplayMessage;
-		string url = "https://img3.akspic.ru/crops/3/4/4/3/7/173443/173443-besplatnyj_minimalist-ubuntu-minimalizm-tsvetnoy-linkin_park-7680x4320.jpg";
-		//string url = "https://effigis.com/wp-content/uploads/2015/02/Iunctus_SPOT5_5m_8bit_RGB_DRA_torngat_mountains_national_park_8bits_1.jpg";
-		string savePath = "/Users/yulia/Desktop/bigimage.jpg";
+		string url = "https://img3.akspic.ru/crops/3/4/4/3/7/173443/173443-besplatnyj_minimalist-ubuntu-minimalizm-tsvetnoy-linkin_park-7680x4320.jpg";		string savePath = "/Users/yulia/Desktop/bigimage.jpg";
 		Task downloadTask = downloader.Download(url,savePath);
 		ConsoleKeyInfo keyInfo;
 		do
