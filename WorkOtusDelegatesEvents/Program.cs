76f9f56537c2178ffa1167c51f345965cba8fee8
yulia <koroleva21.jv@gmail.com>
2024-01-21 22:08:17 +0300

    fix

---
+++ WorkOtusDelegatesEvents/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace WorkOtusDelegatesEvents
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Функция GetMax
+            List<int> numbers = new List<int> { 10, 5, 8, 3, 12 };
+            int maxNumber = numbers.GetMax(x => x);
+            Console.WriteLine("Максимальное число: " + maxNumber);
+            Console.WriteLine(
+                "Введите директорию для поиска файлов, для отмены поиска нажмите пробел"
+            );
+            string filePath = Console.ReadLine();
+            // Класс FileSearcher
+            FileSearcher fileSearcher = new FileSearcher();
+            fileSearcher.FileFound += FileSearcher_FileFound;
+            fileSearcher.SearchCancelled += FileSearcher_SearchCancelled;
+            fileSearcher.SearchFiles(filePath);
+
+            Console.ReadLine();
+        }
+
+        private static void FileSearcher_FileFound(object sender, FileArgs e)
+        {
+            Console.WriteLine("Найден файл: " + e.FileName);
+        }
+
+        private static void FileSearcher_SearchCancelled(object sender, EventArgs e)
+        {
+            Console.WriteLine("Поиск отменен.");
+        }
+    }
+}
